e2b5493ba4968b887a7c4e205ef6a4d2c2b12170
pwojton <piotr.wojton.01@gmail.com>
2023-11-12 16:50:22 +0100

    Change databes saving method

--- database.py
+++ database.py
@@ -10,20 +10,14 @@ db_host = os.environ["DB_HOST"]
 db_port = os.environ["DB_PORT"]
 
 
-def push_counted_pedestrians(count, time):
-    print()
-    # conn = psycopg2.connect(user=db_user, password=db_pass, database=db_name, host=db_host,
-    #                         port=db_port)
-    # cur = conn.cursor()
-    # cur.execute(f"INSERT INTO pedestrians_count (ped_count, time) VALUES ('{count}', '{time}')")
-    # conn.commit()
-
-
-def push_pedestrians_coming_up_or_down(start_time, end_time, coming_up, coming_down):
-    print()
-    # conn = psycopg2.connect(user=db_user, password=db_pass, database=db_name, host=db_host,
-    #                         port=db_port)
-    # cur = conn.cursor()
-    # cur.execute(
-    #     f"INSERT INTO pedestrians_coming_up_or_down (start_time, stop_time, ped_count_up, ped_count_down) VALUES ('{start_time}', '{end_time}', '{coming_up}', '{coming_down}')")
-    # conn.commit()
+def push_pedestrian_track(start_time, stop_time, ped_id, spots, aliases):
+    if len(spots) == 0:
+        spots.append(0)
+    if len(aliases) == 0:
+        aliases.append(0)
+    conn = psycopg2.connect(user=db_user, password=db_pass, database=db_name, host=db_host,
+                            port=db_port)
+    cur = conn.cursor()
+    cur.execute(
+        f"INSERT INTO pedestrians_track (start_time, stop_time, ped_id, spots, aliases) VALUES ('{start_time}', '{stop_time}', '{ped_id}', ARRAY{spots}, ARRAY{aliases})")
+    conn.commit()
